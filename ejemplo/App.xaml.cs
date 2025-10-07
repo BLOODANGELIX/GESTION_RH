3c05d3b25fa987db4b1fa93465d91c1ed83f7ff2
J <juliolorenzoaquinoibarra@gmail.com>
2025-10-07 11:34:42 -0600

    ejemplo descomprimido
    El proyecto ejemplo descomprimido, sin conexión a base de datos

---
+++ ejemplo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ejemplo.view;
+
+namespace ejemplo
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var LoginView = new LoginView();
+            LoginView.Show(); //show para mostrar la ventana
+            LoginView.IsVisibleChanged += (s, ev) =>
+            {
+                if (LoginView.IsVisible == false && LoginView.IsLoaded)
+                {
+                    var mainView = new MainWindow();
+                    mainView.Show();
+                    LoginView.Close();
+                }
+            };
+        }
+    }
+}
---
+++ ejemplo/view/LoginView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ejemplo.view
+{
+
+    /// <summary>
+    /// Lógica de interacción para LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Minimizar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_cerrar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
