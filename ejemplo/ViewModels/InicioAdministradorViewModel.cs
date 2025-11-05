c8b15eeeff8bd81fd4e7f6c7d414884de5e612fd
brayanluna1 <brayanlunaibarra@gmail.com>
2025-11-05 08:17:22 -0600

    Se agrego la forma de cambiar entre pantallasy los data grid, asi como el estilo de este

---
+++ ejemplo/ViewModels/InicioAdministradorViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using FontAwesome.WPF;
+using System.Windows.Input;
+using ejemplo.Views;
+
+namespace ejemplo.ViewModels
+{
+    public class InicioAdministradorViewModel : ViewModelBase
+    {
+        private ViewModelBase _currentView;
+        private string _description;
+        private FontAwesomeIcon _icon;
+
+        public ViewModelBase CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+        public FontAwesomeIcon Icon
+        {
+            get => _icon;
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
+        // Comandos
+        public ICommand ShowEmpleadoViewCommand { get; }
+        public ICommand ShowDepartamentoViewCommand { get; }
+        public ICommand ShowPuestoViewCommand { get; }
+
+        public InicioAdministradorViewModel()
+        {
+            ShowDepartamentoViewCommand = new ViewModelCommand(ExecuteShowDepartamentoView);
+            ShowEmpleadoViewCommand = new ViewModelCommand(ExecuteShowEmpleadoView);
+            ShowPuestoViewCommand = new ViewModelCommand(ExecuteShowPuestoView);
+
+            // Vista por defecto
+            ExecuteShowEmpleadoView(null);
+        }
+
+        private void ExecuteShowPuestoView(object obj)
+        {
+            CurrentView = new PuestosViewModel();
+            Description = "Puestos";
+            Icon = FontAwesomeIcon.IdBadge;
+        }
+
+        private void ExecuteShowEmpleadoView(object obj)
+        {
+            CurrentView = new EmpleadosViewModel();
+            Description = "Empleados";
+            Icon = FontAwesomeIcon.Users;
+        }
+
+        private void ExecuteShowDepartamentoView(object obj)
+        {
+            CurrentView = new DepartamentoViewModel();
+            Description = "Departamentos";
+            Icon = FontAwesomeIcon.Building;
+        }
+    }
+}
--- ejemplo/Views/PuestosView.xaml.cs
+++ ejemplo/Views/PuestosView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using ejemplo.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,19 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ejemplo.Views
 {
     /// <summary>
-    /// Lógica de interacción para InicioAdministrtador.xaml
+    /// Lógica de interacción para PuestosView.xaml
     /// </summary>
-    public partial class InicioAdministrtador : Window
+    public partial class PuestosView : UserControl
     {
-        public InicioAdministrtador()
+        public PuestosView()
         {
             InitializeComponent();
-            DataContext = new InicioAdministradorViewModel();
         }
     }
 }
